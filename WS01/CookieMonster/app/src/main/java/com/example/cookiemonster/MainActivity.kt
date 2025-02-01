fca29f1837f9e72c04c5c76df59394e57a1c710c
Prem Dharshan <whizzkid.dpd@gmail.com>
2025-02-01 15:12:47 +0530

    Add WS01 Cookie Monster

---
+++ WS01/CookieMonster/app/src/main/java/com/example/cookiemonster/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.cookiemonster
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        var state = "hungry"
+        val btn = findViewById<Button>(R.id.button)
+        val desc = findViewById<TextView>(R.id.textView2)
+        val image = findViewById<ImageView>(R.id.imageView)
+
+        btn.setOnClickListener {
+            if (state == "hungry") {
+                desc.text = "I'm so full"
+                image.setImageResource(R.drawable.full)
+                btn.text = "Done"
+                state = "full"
+            }
+            else {
+                desc.text = "I'm so hungry"
+                image.setImageResource(R.drawable.hungry)
+                btn.text = "Eat Cookie"
+                state = "hungry"
+            }
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
